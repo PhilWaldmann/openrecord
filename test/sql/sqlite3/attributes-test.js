80e323aecc4474a264061e27129944562046103d
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-02-04 19:41:38 +0100

    fixes #1

--- test/sql/sqlite3/attributes-test.js
+++ test/sql/sqlite3/attributes-test.js
@@ -13,6 +13,7 @@ describe('SQLite3: Attributes', function(){
   before(function(next){
     beforeSql(db_file, [
       'CREATE TABLE attribute_tests(text_attribute  TEXT, numeric_attribute NUMERIC, integer_attribute  INTEGER, real_attribute  REAL, blob_attribute BLOB)',
+      'CREATE TABLE attribute_lowercase_tests(text_attribute  text, numeric_attribute numeric, integer_attribute  integer, real_attribute  real, blob_attribute blob)',
       'CREATE TABLE users(id  INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT NOT NULL, email TEXT)',
       'CREATE TABLE multiple_keys(id  INTEGER, id2 INTEGER, PRIMARY KEY(id, id2))'
     ], next);
@@ -25,6 +26,7 @@ describe('SQLite3: Attributes', function(){
     });
 
     store.Model('AttributeTest', function(){});
+    store.Model('AttributeLowercaseTest', function(){});
     store.Model('User', function(){});
     store.Model('MultipleKey', function(){});
   });
@@ -59,15 +61,49 @@ describe('SQLite3: Attributes', function(){
 
       var attrs = AttributeTest.definition.attributes;
       
-      attrs.text_attribute.type.name.indexOf('TEXT').should.not.be.equal(-1);
-      attrs.numeric_attribute.type.name.indexOf('NUMERIC').should.not.be.equal(-1);
-      attrs.integer_attribute.type.name.indexOf('INTEGER').should.not.be.equal(-1);
-      attrs.real_attribute.type.name.indexOf('REAL').should.not.be.equal(-1);
-      attrs.blob_attribute.type.name.indexOf('BLOB').should.not.be.equal(-1);
+      attrs.text_attribute.type.name.indexOf('text').should.not.be.equal(-1);
+      attrs.numeric_attribute.type.name.indexOf('numeric').should.not.be.equal(-1);
+      attrs.integer_attribute.type.name.indexOf('integer').should.not.be.equal(-1);
+      attrs.real_attribute.type.name.indexOf('real').should.not.be.equal(-1);
+      attrs.blob_attribute.type.name.indexOf('blob').should.not.be.equal(-1);
     
       done();
     });
   });
+  
+  
+  it('have all attributes loaded (lowercase)', function(done){
+    store.ready(function(){    
+      var AttributeLowercaseTest = store.Model('AttributeLowercaseTest');
+
+      var attrs = AttributeLowercaseTest.definition.attributes;
+      
+      attrs.should.have.property('text_attribute');
+      attrs.should.have.property('numeric_attribute');
+      attrs.should.have.property('integer_attribute');
+      attrs.should.have.property('real_attribute');
+      attrs.should.have.property('blob_attribute');
+    
+      done();
+    });
+  });
+  
+  it('have the right attribute types (lowercase)', function(done){
+    store.ready(function(){    
+      var AttributeLowercaseTest = store.Model('AttributeLowercaseTest');
+
+      var attrs = AttributeLowercaseTest.definition.attributes;
+      
+      attrs.text_attribute.type.name.indexOf('text').should.not.be.equal(-1);
+      attrs.numeric_attribute.type.name.indexOf('numeric').should.not.be.equal(-1);
+      attrs.integer_attribute.type.name.indexOf('integer').should.not.be.equal(-1);
+      attrs.real_attribute.type.name.indexOf('real').should.not.be.equal(-1);
+      attrs.blob_attribute.type.name.indexOf('blob').should.not.be.equal(-1);
+    
+      done();
+    });
+  });
+  
   
   
   it('has the right primary_key', function(done){
