72d4ea9b252f44a12e081cc5e1f0477db19a7de8
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-02-06 22:53:39 +0100

    start auto documentation

---
+++ documentation/generate.js
@@ -0,0 +1,51 @@
+var markdox = require('markdox');
+var glob = require('glob');
+
+
+
+
+function documentation(class_name){
+  
+  class_name = class_name.toLowerCase();
+  
+  var options = {
+    output: __dirname + '/' + class_name + '.md',
+  
+    formatter: function(docfile){
+      var tmp = [];
+      
+      for(var i in docfile.javadoc){
+        var incl = false;
+        var area = docfile.javadoc[i];
+        
+        for(var t in area.raw.tags){
+          var tag = area.raw.tags[t];
+          
+          if(tag.type == 'class' && tag.string.toLowerCase() == class_name){
+            incl = true;
+          }
+        }
+        
+        if(incl){
+          tmp.push(area);
+        }
+      }
+      
+      //console.log(docfile);
+      
+      docfile.javadoc = tmp;
+      docfile.model = class_name.replace(/^(.)/, function(a){return a.toUpperCase();})
+      return docfile;
+    },
+  
+    template: __dirname + '/template.ejs'
+  };
+
+  markdox.process(glob.sync('./lib/**/*.js'), options, function(){
+    console.log('Documentation generated');
+  });
+  
+}
+
+
+documentation('model');
---
+++ documentation/model.md
@@ -0,0 +1,22 @@
+# Model
+
+## join(relation, type)
+
+Joins one or many relations with the current model
+
+### Params: 
+
+* **string** *relation* - The relation name which should be joined.
+
+* **string** *type* - Optional join type (Allowed are `left`, `inner`, `outer` and `right`).
+
+## limit(limit, offset)
+
+Limit the resultset to `n` records
+
+### Params: 
+
+* **integer** *limit* - The limit as a number.
+
+* **integer** *offset* - Optional offset.
+
