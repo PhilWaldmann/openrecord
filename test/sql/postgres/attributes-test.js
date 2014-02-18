7091cd3cffe3302924d7045ad73d785916e65fb8
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-02-18 16:09:20 +0100

    postgres date, datetime und time bugfix

--- test/sql/postgres/attributes-test.js
+++ test/sql/postgres/attributes-test.js
@@ -11,7 +11,8 @@ describe('Postgres: all Attributes', function(){
   
   before(function(next){
     beforePG(database, [
-      'CREATE TABLE attribute_tests(char_attribute  varchar(255), float_attribute float, integer_attribute  integer, text_attribute text, boolean_attribute boolean, binary_attribute bytea, date_attribute date, datetime_attribute timestamp, time_attribute time)'
+      'CREATE TABLE attribute_tests(char_attribute  varchar(255), float_attribute float, integer_attribute  integer, text_attribute text, boolean_attribute boolean, binary_attribute bytea, date_attribute date, datetime_attribute timestamp, time_attribute time)',
+      "INSERT INTO attribute_tests VALUES('abcd', 2.3345, 3243, 'some text', true, 'some binary data', '2014-02-18', '2014-02-18 15:45:02', '15:45:01')"
     ], next);
   });
   
@@ -54,7 +55,26 @@ describe('Postgres: all Attributes', function(){
     
       done();
     });
-  }); 
+  });
   
+  
+  it('casts all values', function(done){
+    store.ready(function(){    
+      var AttributeTest = store.Model('AttributeTest');
+      AttributeTest.limit(1).exec(function(record){
+        record.char_attribute.should.be.equal('abcd');
+        record.float_attribute.should.be.equal(2.3345);
+        record.integer_attribute.should.be.equal(3243);
+        record.text_attribute.should.be.equal('some text');
+        record.boolean_attribute.should.be.equal(true);
+        record.binary_attribute.should.be.eql(new Buffer('some binary data', 'utf-8'));
+        record.date_attribute.toString().should.be.equal('2014-02-18');
+        record.datetime_attribute.toJSON().should.be.equal('2014-02-18T14:45:02.000Z');
+        record.time_attribute.toString().should.be.equal('15:45:01');
+        done();
+      });
+    });
+  });
+    
 });
 
