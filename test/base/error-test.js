dd684dc00d4a5e820d80ec295d473a7822a02c80
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-05-07 14:35:12 +0200

    merge relation errors

--- test/base/error-test.js
+++ test/base/error-test.js
@@ -6,13 +6,31 @@ describe('Error', function(){
   var store = new Store();
 
   store.Model('User', function(){
-        
+    this.hasMany('posts');
+    this.belongsTo('avatar');
+  });
+  
+  store.Model('Post', function(){
+    this.attribute('title');
+  });
+  
+  store.Model('Avatar', function(){
+    this.attribute('url');
   });
   
   var User = store.Model('User');
   var phil = new User();
-  
-  
+  var michl = new User({
+    posts:[{
+      title: 'foo'
+    }],
+    
+    avatar: {
+      url: 'http://foo.com/img.png'
+    }
+  });
+
+
   describe('errors.add()', function(){
     it('add attribute error', function(){
       phil.errors.add('attribute_name', 'not valid');
@@ -31,7 +49,32 @@ describe('Error', function(){
     it('has base errors', function(){
       phil.errors['base'].should.be.an.instanceOf(Array);
       phil.errors['base'][0].should.be.equal('can not be deleted');
-    })
+    });
+    
+  });
+  
+  
+  describe('relation errors', function(){
+
+    michl.posts[0].errors.add('title', 'some title error');
+    michl.avatar.errors.add('url', 'some url error');
+
+    it('has the posts error object on its error obj.', function(){
+      michl.errors.should.have.property('posts');
+    });
+    
+    it('has the avatar error object on its error obj.', function(){
+      michl.errors.should.have.property('avatar');
+    });
+    
+    it('errors is an array for hasMany', function(){
+      michl.errors.posts.should.be.instanceOf(Array);
+    });
+    
+    it('errors is an object for belongsTo', function(){
+      michl.errors.avatar.should.not.be.instanceOf(Array);
+    });
+    
   });
   
 });
