2aee719017400d34efaf14cca5e981f1f0238740
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-11-03 16:30:48 +0100

    almost fixed all tests

--- test/sql/__shared/includes-test.js
+++ test/sql/__shared/includes-test.js
@@ -260,7 +260,7 @@ module.exports = function(title, beforeFn, afterFn, store_conf){
       it('returns the right results on hasMany through', function(next){ 
         store.ready(function(){
           var User = store.Model('User');
-          User.include('unread').order('users.id').exec(function(result){
+          User.include('unread').order('users.id').exec(function(result){   
             result.length.should.be.equal(3);
             result[0].unread.length.should.be.equal(1);
             result[1].unread.length.should.be.equal(0);
@@ -274,7 +274,7 @@ module.exports = function(title, beforeFn, afterFn, store_conf){
       it('returns the right results on nested hasMany through', function(next){ 
         store.ready(function(){
           var User = store.Model('User');
-          User.include('unread_threads').order('users.id').exec(function(result){
+          User.include('unread_threads').order('users.id').exec(function(result){   
             result.length.should.be.equal(3);
             result[0].unread_threads.length.should.be.equal(1);
             result[0].unread_threads[0].title.should.be.equal('second thread');
@@ -347,7 +347,6 @@ module.exports = function(title, beforeFn, afterFn, store_conf){
         store.ready(function(){
           var User = store.Model('User');
           User.include({threads:{user:'unread_threads'}}).where({threads:{user:{unread_threads:["title like ?", 'second%']}}}).order('users.id').exec(function(result){
-            console.log(result);
             result.length.should.be.equal(3);
             result[0].unread_threads.length.should.be.equal(0);
             result[1].unread_threads.length.should.be.equal(0);
@@ -461,10 +460,11 @@ module.exports = function(title, beforeFn, afterFn, store_conf){
       
       
       
-      it('returns a the result + the totalCount', function(next){
+      it('returns the result + the totalCount', function(next){
         store.ready(function(){
           var User = store.Model('User');
           User.limit(2).include(':totalCount').exec(function(result){
+            console.log('>>', result.$totalCount);
             result.length.should.be.equal(2);
             result.$totalCount.should.be.equal(3);
             next();
