2f4c5f81fc94897c36b9e6bf5bcc2217deecd5ee
Philipp Waldmann <philipp@wu-server.com>
2017-06-03 14:19:14 +0200

    test and bugfix for #38

---
+++ test/sql/postgres/uuid_key-test.js
@@ -0,0 +1,53 @@
+var should = require('should');
+var Store = require('../../../lib/store');
+
+
+describe('Postgres: UUID Key', function(){
+  var store;
+  var database = 'uuid_key_test';
+
+
+
+  before(function(next){
+    this.timeout(5000);
+    beforePG(database, [
+      'CREATE EXTENSION IF NOT EXISTS \\"uuid-ossp\\"',
+      'CREATE TABLE uuid_tests (id uuid not null primary key default uuid_generate_v1(), another_column varchar(255))'
+    ], next);
+  });
+
+  before(function(){
+    store = new Store({
+      host: 'localhost',
+      type: 'postgres',
+      database: database,
+      user: 'postgres',
+      password: ''
+    });
+
+    store.Model('UuidTest', function(){});
+
+    store.setMaxListeners(0);
+    store.on('exception', function(){});
+  });
+
+  after(function(next){
+    afterPG(database, next);
+  });
+
+
+  it('create() returns a new id (uuid)', function(next){
+    store.ready(function(){
+      var UuidTest = store.Model('UuidTest');
+      var test = new UuidTest({ another_column: 'i am setting uuid' })
+
+      test.save(function (success) {
+        console.log(success, test, test.errors)
+        should.exist(test.id)
+        success.should.be.true
+        next();
+      })
+    });
+  });
+
+});
