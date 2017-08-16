0e9aae602608cbee34eff81299145b2323881940
Philipp Waldmann <philipp@wu-server.com>
2017-08-16 17:03:19 +0200

    fix sqlplus cmd

--- test/sql/oracle/__helper.js
+++ test/sql/oracle/__helper.js
@@ -1,13 +1,13 @@
 var exec = require('child_process').exec
 
 global.beforeOracle = function(db, sql, next){
-  exec('$ORACLE_HOME/bin/sqlplus -L -S / AS SYSDBA "DROP DATABASE ' + db + '"', function(err, result){ // eslint-disable-line
+  exec('sqlplus -L -S / AS SYSDBA << "DROP DATABASE ' + db + '"', function(err, result){ // eslint-disable-line
     console.log('DROP', db, result)
     if(err) console.log('ORACLE', err)
-    exec('$ORACLE_HOME/bin/sqlplus -L -S / AS SYSDBA "create database ' + db + '"', function(err){ // eslint-disable-line
+    exec('sqlplus -L -S / AS SYSDBA << "create database ' + db + '"', function(err){ // eslint-disable-line
       console.log('CREATE', db, result)
       if(err) console.log('ORACLE', err)
-      exec('$ORACLE_HOME/bin/sqlplus -L -S / AS SYSDBA "USE ' + db + ';' + sql.join(';') + '"', function(err, result){
+      exec('sqlplus -L -S / AS SYSDBA << "USE ' + db + ';' + sql.join(';') + '"', function(err, result){
         console.log('EXEC', db, result)
         if(err) throw new Error(err)
         next()
