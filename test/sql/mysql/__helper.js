e052e42bff8030eaaebabfcbc6ffbd420d452183
Philipp Waldmann <philipp@wu-server.com>
2017-08-17 18:21:31 +0200

    do we have to use root now?!

--- test/sql/mysql/__helper.js
+++ test/sql/mysql/__helper.js
@@ -1,9 +1,9 @@
 var exec = require('child_process').exec
 
 global.beforeMYSQL = function(db, sql, next){
-  exec('mysql -u travis -e "DROP DATABASE ' + db + '"', function(err, result){ // eslint-disable-line
-    exec('mysql -u travis -e "create database ' + db + '"', function(err){ // eslint-disable-line
-      exec('mysql ' + db + ' -e "' + sql.join(';') + '" -u travis', function(err, result){
+  exec('mysql -u root -e "DROP DATABASE ' + db + '"', function(err, result){ // eslint-disable-line
+    exec('mysql -u root -e "create database ' + db + '"', function(err){ // eslint-disable-line
+      exec('mysql ' + db + ' -e "' + sql.join(';') + '" -u root', function(err, result){
         if(err) throw new Error(err)
         next()
       })
