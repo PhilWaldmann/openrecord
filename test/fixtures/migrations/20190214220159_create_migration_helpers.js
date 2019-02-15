b8a81425d573c4d62f252a658ff24498137e17bb
Steve Shreeve <steve.shreeve@gmail.com>
2019-02-14 23:36:58 -0700

    Commit some files for testing (need to get them to work)
    I'm trying to follow the standard testing files, but couldn't figure out how to get it to work (yet?).

---
+++ test/fixtures/migrations/20190214220159_create_migration_helpers.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+  this.createTable('migration_helpers', function () {
+    this.string('string_attr', 42) // includes string len
+    this.string('string_attr_def', 69, { default: 'hrm' }) // adds default value
+    this.string('req_string_attr!', 13, { default: 'nice' }) // adds not null
+
+    this.id('foreign_id') // id is an integer
+    this.id('req_foreign_id!') // adds not null
+
+    this.index('string_attr') // adds index
+    this.unique('req_string_attr') // adds unique
+  })
+}
